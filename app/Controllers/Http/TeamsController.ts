32c99e1087e1376809f6b50b16b1e4afffa6999b
Matúš Močkor <codex611@gmail.com>
2023-01-15 23:54:24 +0100

    CHANGED: use route with controller

---
+++ app/Controllers/Http/TeamsController.ts
@@ -0,0 +1,7 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class TeamsController {
+  public async index() {
+    return { haha: 'muhaha' }
+  }
+}
--- start/team.ts
+++ start/team.ts
@@ -1,7 +1,5 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-  Route.get('test', async () => {
-    return { haha: 'test' }
-  })
+  Route.get('/', 'TeamsController.index')
 }).prefix('team')
