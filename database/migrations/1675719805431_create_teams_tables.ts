576f77c1b3d249fef258e14e80043e70f1d194e4
Matúš Močkor <matus.mockor@gmail.com>
2023-02-08 19:01:13 +0100

    CHANGED: team body should be text not only string

--- database/migrations/1675719805431_create_teams_tables.ts
+++ database/migrations/1675719805431_create_teams_tables.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id');
       table.string('name');
-      table.string('body');
+      table.text('body');
       table.string('slug');
 
       /**
